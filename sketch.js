cd169b2b3056f6ffd8f783ea8efd9ee43367f535
Nilanjan2005 <74534727+Nilanjan2005@users.noreply.github.com>
2021-04-12 21:19:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,12 +7,14 @@ var END =0;
 var PLAY =1;
 var gameState = PLAY;
 var score = 0;
+var gameOver,gameOverImage;
 
 function preload(){
   backImage=loadImage("jungle.jpg");
   player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
 bananaImage = loadImage("banana.png");
 obstaclesImage = loadImage("stone.png");
+gameOverImage = loadImage("gameOver.png")
 }
 
 function setup() {
@@ -32,6 +34,7 @@ function setup() {
   ground.x=ground.width/2;
   ground.visible=false;
   
+
   bananaGroup = new Group();
   obstaclesGroup = new Group();
 }
@@ -72,9 +75,11 @@ player.visible = false;
 bananaGroup.destroyEach();
 obstaclesGroup.destroyEach();
 
-textSize(30);
-fill("white");
-text("Game Over!",300,200)
+gameOver = createSprite(300,200);
+gameOver.addImage(gameOverImage);
+//textSize(30);
+//fill("white");
+//text("Game Over!",300,200)
 
 }
 
@@ -89,7 +94,7 @@ if(frameCount%80 === 0 ){
   var banana = createSprite(600,250,40,10);
   banana.y = Math.round( random(120,200));
   banana.addImage(bananaImage);
-banana.scale = 0.10;
+banana.scale = 0.05;
 banana.velocityX = -6;
 banana.lifeTime = 250;
 player.depth = banana.depth+1;
